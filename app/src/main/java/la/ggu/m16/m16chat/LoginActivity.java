ab66f807a64a49eaa92bca5451beb78fd3f9bce0
DingGGu <admin@gnetwork.kr>
2015-07-01 16:07:30 +0900

    activity_login class

--- app/src/main/java/la/ggu/m16/m16chat/LoginActivity.java
+++ app/src/main/java/la/ggu/m16/m16chat/LoginActivity.java
@@ -1,38 +1,39 @@
 package la.ggu.m16.m16chat;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity implements View.OnClickListener {
+    private EditText login_username, login_password;
+    private Button login_submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_username = (EditText) findViewById(R.id.login_username);
+        login_password = (EditText) findViewById(R.id.login_password);
+        login_submit = (Button) findViewById(R.id.login_submit);
+        login_submit.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        if(v.getId() == login_submit.getId()) {
+            String username = login_username.getText().toString();
+            String password = login_password.getText().toString();
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(LoginActivity.this, "아이디 비밀번호를 입력하지 않았어요.", Toast.LENGTH_SHORT).show();
+            } else {
+                startActivity(new Intent(this, ChatActivity.class));
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
