e4ada2d00ef1a6eaf6949d2a4913efba8e900009
DingGGu <admin@gnetwork.kr>
2015-07-18 13:41:03 +0900

    ClanMember List

--- app/src/main/java/bnetp/clan/ClanMemberStatusFlags.java
+++ app/src/main/java/bnetp/clan/ClanMemberStatusFlags.java
@@ -1,6 +1,10 @@
 package bnetp.clan;
 
 public class ClanMemberStatusFlags {
-    public static final byte CLANMEMBERSTATUS_OFFLINE   = (byte)0x00;
-    public static final byte CLANMEMBERSTATUS_ONLINE    = (byte)0x01;
+    public static final byte CLANMEMBERSTATUS_OFFLINE                       = (byte)0x00;
+    public static final byte CLANMEMBERSTATUS_NOT_IN_CHAT					= (byte)0x01;
+    public static final byte CLANMEMBERSTATUS_IN_CHAT						= (byte)0x02;
+    public static final byte CLANMEMBERSTATUS_IN_A_PUBLIC_GAME				= (byte)0x03;
+    public static final byte CLANMEMBERSTATUS_IN_A_PRIVATE_GAME_NOT_MUTUAL	= (byte)0x04;
+    public static final byte CLANMEMBERSTATUS_IN_A_PRIVATE_GAME_MUTUAL		= (byte)0x05;
 }
--- app/src/main/java/la/ggu/m16/m16chat/cv/ClanMemberAdapter.java
+++ app/src/main/java/la/ggu/m16/m16chat/cv/ClanMemberAdapter.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import bnetp.clan.ClanMember;
+import bnetp.clan.ClanMemberRankIDs;
+import bnetp.clan.ClanMemberStatusFlags;
 import la.ggu.m16.m16chat.R;
 import la.ggu.m16.m16chat.util.ParseUsername;
 
@@ -26,14 +28,80 @@ public class ClanMemberAdapter extends ArrayAdapter<ClanMember> {
     @Override
     public View getView (int position, View convertView, ViewGroup parent) {
         View v = convertView;
-        if (v == null) {
-            LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            v = vi.inflate(R.layout.custom_channel_user, null);
-        }
+        LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         ClanMember cm = ClanMembers.get(position);
         if (cm != null) {
-            TextView text1 = (TextView) v.findViewById(R.id.channel_user_list_item);
-            text1.setText(ParseUsername.parseColor(cm.username));
+            switch(cm.online) {
+                case ClanMemberStatusFlags.CLANMEMBERSTATUS_OFFLINE: {
+                    v = vi.inflate(R.layout.custom_channel_user, null);
+                    TextView text1 = (TextView) v.findViewById(R.id.channel_user_list_item);
+
+                    switch(cm.rank) {
+                        case ClanMemberRankIDs.CLANMEMBERRANK_CHIEFTAIN: {
+                            text1.setTextColor(0xFF0043F5);
+                            break;
+                        }
+                        case ClanMemberRankIDs.CLANMEMBERRANK_SHAMAN: {
+                            text1.setTextColor(0xFFBB110F);
+                            break;
+                        }
+                        case ClanMemberRankIDs.CLANMEMBERRANK_GRUNT: {
+                            text1.setTextColor(0xFF097e00);
+                            break;
+                        }
+                    }
+                    text1.setText(ParseUsername.parseColor(cm.username));
+                    break;
+                }
+                default: {
+                    v = vi.inflate(R.layout.custom_channel_user_online, null);
+                    TextView text1 = (TextView) v.findViewById(R.id.channel_user_list_item);
+                    TextView text2 = (TextView) v.findViewById(R.id.channel_user_list_item_location);
+                    String locationText = cm.location;
+
+                    switch(cm.rank) {
+                        case ClanMemberRankIDs.CLANMEMBERRANK_CHIEFTAIN: {
+                            text1.setTextColor(0xFF0043F5);
+                            break;
+                        }
+                        case ClanMemberRankIDs.CLANMEMBERRANK_SHAMAN: {
+                            text1.setTextColor(0xFFBB110F);
+                            break;
+                        }
+                        case ClanMemberRankIDs.CLANMEMBERRANK_GRUNT: {
+                            text1.setTextColor(0xFF097e00);
+                            break;
+                        }
+                    }
+                    text1.setText(cm.username);
+
+                    switch (cm.online) {
+                        case ClanMemberStatusFlags.CLANMEMBERSTATUS_NOT_IN_CHAT: {
+                            locationText = "접속 중";
+                            break;
+                        }
+                        case ClanMemberStatusFlags.CLANMEMBERSTATUS_IN_CHAT: {
+                            locationText = "채널 (" + locationText + ")에 참여 중";
+                            break;
+                        }
+                        case ClanMemberStatusFlags.CLANMEMBERSTATUS_IN_A_PUBLIC_GAME: {
+                            locationText = "공개 게임 (" + locationText + ")에 참여 중";
+                            break;
+                        }
+                        case ClanMemberStatusFlags.CLANMEMBERSTATUS_IN_A_PRIVATE_GAME_MUTUAL: {
+                            locationText = "비공개 게임 (" + locationText + ")에 참여 중";
+                            break;
+                        }
+                        case ClanMemberStatusFlags.CLANMEMBERSTATUS_IN_A_PRIVATE_GAME_NOT_MUTUAL: {
+                            locationText = "비공개 게임 중";
+                            break;
+                        }
+                    }
+                    
+                    text2.setText(locationText);
+                    break;
+                }
+            }
         }
         return v;
     }
